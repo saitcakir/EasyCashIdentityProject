a5f59f5681ccec6c92c659a29ee0031ac7ef1fbd
saitcakir <sait25@windowslive.com>
2023-07-20 14:29:46 +0300

    CustomerLayout ViewComponentler oluşturuldu.

---
+++ EasyCashIdentityProject.PresentationLayer/ViewComponents/Customer/_CustomerLayoutNavbarPartial.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace EasyCashIdentityProject.PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutNavbarPartial:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

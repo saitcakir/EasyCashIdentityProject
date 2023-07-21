8335b8f0908fdb883ed27f7607694a21679cc8c9
saitcakir <sait25@windowslive.com>
2023-07-21 11:50:01 +0300

    Layout düzenlemeleri yapıldı.

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/MyAccountsController.cs
@@ -0,0 +1,25 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    [Authorize]
+    public class MyAccountsController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public MyAccountsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            return View();
+        }
+    }
+}
--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -59,7 +59,7 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
 
 					SmtpClient client = new SmtpClient();
 					client.Connect("smtp.gmail.com",587,false);
-					client.Authenticate("saitcakir25@gmail.com", "PassCode");
+					client.Authenticate("saitcakir25@gmail.com", "wzwbypslluleaujc");
 					client.Send(mimeMessage);
 					client.Disconnect(true);
 
